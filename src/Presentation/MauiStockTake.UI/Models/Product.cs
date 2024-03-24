213c8e0cfb1c8f75140a4fb5720e0b004874e05b
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2024-03-24 13:36:40 +0100

    finished chapter 7

---
+++ src/Presentation/MauiStockTake.UI/Models/Product.cs
@@ -0,0 +1,9 @@
+namespace MauiStockTake.UI.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int ManufacturerId { get; set; }
+    public string ManufacturerName { get; set; }
+}
---
+++ src/Presentation/MauiStockTake.UI/Pages/InputPage.xaml.cs
@@ -0,0 +1,23 @@
+using MauiStockTake.UI.Models;
+
+namespace MauiStockTake.UI.Pages;
+
+public partial class InputPage : ContentPage
+{
+    public InputPage()
+    {
+        InitializeComponent();
+    }
+    
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+        var product = new Product { Name = "MauiStockTake", ManufacturerName = "BeachBytes" };
+ 
+        var pageParams = new Dictionary<string, object>
+        {
+            { "Product", product }
+        };
+ 
+        await Shell.Current.GoToAsync("productdetails", pageParams);
+    }
+}
